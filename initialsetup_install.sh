1c1dc088c7fd45dd9b688b56fe61c2dd94e62dab
hometinker12 <48232506+hometinker12@users.noreply.github.com>
2019-07-22 23:54:12 -0400

    Update initialsetup_install.sh

--- initialsetup_install.sh
+++ initialsetup_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DATA_SHARE=/usr/share
 NGINX_CONFIG=/usr/share
-OVPN_DATA="ovpn-data"
+OVPN_DATA=/usr/share/ovpn-data
 OVPN_URL="ha.bedards.net"
 
 
@@ -51,12 +51,17 @@ docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.
 
 ### OpenVPN
 echo "[Info] Install OpenVPN..."
-docker volume create --name $OVPN_DATA
+mkdir $OVPN_DATA
+echo "[Info] Create OpenVPN config..."
 docker run -v $OVPN_DATA:/etc/openvpn --rm giggio/openvpn-arm ovpn_genconfig -u udp://$OVPN_URL
+echo "[Info] Install OpenVPN server certificate..."
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it giggio/openvpn-arm ovpn_initpki nopass
-
+echo "[Info] Start OpenVPN..."
 docker run -v $OVPN_DATA:/etc/openvpn -d --name openvpn -p 1194:1194/udp --cap-add=NET_ADMIN giggio/openvpn-arm
 
-###Get VPN Client Cert
-#docker run -v $OVPN_DATA:/etc/openvpn --rm -it giggio/openvpn-arm easyrsa build-client-full CLIENTNAME nopass
-#docker run -v $OVPN_DATA:/etc/openvpn --rm giggio/openvpn-arm ovpn_getclient CLIENTNAME > CLIENTNAME.ovpn
+### Get VPN Client Cert
+echo "[Info] Generate OpenVPN client cert..."
+mkdir $OVPN_DATA/clientcert
+read -p "Enter the name of your pc client: " CLIENTNAME
+docker run -v $OVPN_DATA/clientcert:/etc/openvpn --rm -it giggio/openvpn-arm easyrsa build-client-full $CLIENTNAME nopass
+docker run -v $OVPN_DATA/clientcert:/etc/openvpn --rm giggio/openvpn-arm ovpn_getclient $CLIENTNAME > $CLIENTNAME.ovpn
