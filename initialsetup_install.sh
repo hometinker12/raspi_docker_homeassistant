160ba8f598d78248bdd4d2059c8b0834f507a7a1
hometinker12 <48232506+hometinker12@users.noreply.github.com>
2019-07-25 22:17:29 -0400

    Update initialsetup_install.sh

--- initialsetup_install.sh
+++ initialsetup_install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-DATA_SHARE=/usr/share/dockerconfigs
-OVPN_DATA=/ovpn-data
-PORTAINER_DATA=/ovpn-data
+DATA_SHARE=/usr/share
+PORTAINER_DATA=/portainer
 HASSIO_DATA=/hassio
-OVPN_URL="ha.bedards.net"
-SMB_USER="pi"
-HOST_NAME="ha-host"
 
 
 ### Check root permissions
@@ -16,6 +12,7 @@ if [ "$EUID" -ne 0 ]
 fi
 
 ### Update Hostname
+read -p "Enter a new hostname for your docker host: " HOST_NAME
 sed -i "s/raspberrypi/$HOST_NAME/" /etc/hostname
 sed -i "s/raspberrypi/$HOST_NAME/" /etc/hosts
 
@@ -47,35 +44,3 @@ echo "[Info] Install portainer..."
 echo "[Info] Create portainer config location ($DATA_SHARE$PORTAINER_DATA)..."
 docker volume create $DATA_SHARE$PORTAINER_DATA/
 docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v $DATA_SHARE$PORTAINER_DATA:/data portainer/portainer
-
-
-### OpenVPN
-echo "[Info] Install OpenVPN..."
-echo "[Info] Create openvpn config location ($DATA_SHARE$OVPN_DATA)..."
-mkdir $DATA_SHARE$OVPN_DATA
-echo "[Info] Create OpenVPN config..."
-docker run -v $DATA_SHARE$OVPN_DATA:/etc/openvpn --rm giggio/openvpn-arm ovpn_genconfig -u udp://$OVPN_URL
-
-### Run Commands
-echo "[Info] Generate OpenVPN addition ttyl commands to run..."
-mkdir $DATA_SHARE$OVPN_DATA/clientcert
-read -p "Enter the name of your pc client: " CLIENTNAME
-echo "---Run These Commands Individually---"
-echo "docker run -v $DATA_SHARE$OVPN_DATA:/etc/openvpn --rm -it giggio/openvpn-arm ovpn_initpki nopass"
-echo "docker run -v $DATA_SHARE$OVPN_DATA:/etc/openvpn -d --name openvpn -p 1194:1194/udp --cap-add=NET_ADMIN giggio/openvpn-arm"
-echo "docker run -v $DATA_SHARE$OVPN_DATA:/etc/openvpn --rm -it giggio/openvpn-arm easyrsa build-client-full $CLIENTNAME nopass"
-echo "docker run -v $DATA_SHARE$OVPN_DATA:/etc/openvpn --rm giggio/openvpn-arm ovpn_getclient $CLIENTNAME > $CLIENTNAME.ovpn"
-
-
-
-
-### Authelia  !!!needswork
-#mkdir $DATA_SHARE/authelia
-#wget https://raw.githubusercontent.com/clems4ever/authelia/master/config.template.yml -O $DATA_SHARE/authelia/config.yml
-#docker run -v $DATA_SHARE/authelia/config.yml:/etc/authelia/config.yml clems4ever/authelia
-
-
-### NGINX  !!!needswork
-#mkdir $DATA_SHARE/nginx/
-#wget https://raw.githubusercontent.com/clems4ever/authelia/master/config.template.yml -O $DATA_SHARE/authelia/config.yml
-#docker run --name my-custom-nginx-container -v /host/path/nginx.conf:/etc/nginx/nginx.conf:ro -d nginx
