4510a41b1ae70e4ac91d50919369ac5c55b8f75b
hometinker12 <48232506+hometinker12@users.noreply.github.com>
2020-03-26 18:11:40 -0400

    Update initialsetup_install.sh

--- rpi3b_setup/initialsetup_install.sh
+++ rpi3b_setup/initialsetup_install.sh
@@ -16,9 +16,21 @@ read -p "Enter a new hostname for your docker host: " HOST_NAME
 sed -i "s/raspberrypi/$HOST_NAME/" /etc/hostname
 sed -i "s/raspberrypi/$HOST_NAME/" /etc/hosts
 
-### Create config share
-echo "[Info] Create docker shared config directory ($DATA_SHARE)..."
-mkdir $DATA_SHARE
+### Mount SDA1 to /Docker
+echo "[Info] -----Docker Data Mount to External Drive (/dev/sda1)-----"
+read -p "Create DATA_SHARE ($DATA_SHARE) (y/n) " UPDATE_DOCKER
+if [[ "$UPDATE_DOCKER" =~ ^([yY][eE][sS]|[yY])$ ]]
+    read -p "Mount /dev/sda1 to $DATA_SHARE (y/n) " UPDATE_DOCKER
+    if [[ "$UPDATE_DOCKER" =~ ^([yY][eE][sS]|[yY])$ ]]
+            then echo "[Warn] Executing: get.docker.com -o get-docker.sh && sh get-docker.sh...."
+                    mkdir $DATA_SHARE
+                    mount /dev/sda1 $DATA_SHARE
+                    df -H
+                    echo "/dev/sda1 /docker ext4 rw,relatime,stripe=1024 0 0" &>> /etc/fstab
+            else echo "[Info] Create Docker Data Folder (mkdir $DATA_SHARE)"
+                    mkdir $DATA_SHARE
+    fi
+fi
 
 
 ### Install Docker
@@ -47,4 +59,3 @@ docker volume create $DATA_SHARE$PORTAINER_DATA/
 docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v $DATA_SHARE$PORTAINER_DATA:/data portainer/portainer
 #### Portainer SSL ##
 #docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v $DATA_SHARE$PORTAINER_DATA:/data -v $DATA_SHARE$HASSIO_DATA/ssl:/ssl portainer/portainer --ssl --sslcert /ssl/certChain.pem --sslkey /ssl/certKey.pem
-
